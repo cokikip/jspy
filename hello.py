9a01dd4e674b5b9667dd65151851dc608c7b7f97
coki <cokicollinji@gmail.com>
2019-07-16 12:19:53 -0700

    Python and javascript Tutorial

---
+++ hello.py
@@ -0,0 +1,23 @@
+###print("hello")
+h=10
+if(h>9):
+    print("hello")
+else:
+    print("add new")
+
+name = input("What is your name? ")
+fav_color= input("What is your favarite color? ")
+print(name+" likes "+fav_color)
+
+##Tutorial number 4
+birth_year = input("Birth day year : ")
+print(type(birth_year))
+age = 2019 - int(birth_year)
+print(age)
+
+para = '''
+helo the name is collins
+fav movies is Incredibles
+favrite anime is One peice
+'''
+print(para)
