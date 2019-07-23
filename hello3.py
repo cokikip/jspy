d214cab709754ae6add03cfcf6f584253cd83905
coki <cokicollinji@gmail.com>
2019-07-23 12:20:34 -0700

    4th tutorial

---
+++ hello3.py
@@ -0,0 +1,12 @@
+import math
+
+print(math.ceil(4.2))
+print(math.floor(2.9))
+
+is_hot = True
+if is_hot:
+    print("It's hot enjoy your day")
+    print("Drint lots of water")
+else:
+    print("Ware heavy clothing")
+
