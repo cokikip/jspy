cfed9268a7881f8fb7d143f2d90f72d5acec1fa7
coki <cokicollinji@gmail.com>
2019-07-23 12:05:54 -0700

    3rd tutorial

---
+++ hello2.py
@@ -0,0 +1,14 @@
+course="Python's Course for beginners"
+print(len(course))
+print("Python's" in course)
+
+print(course.upper())
+print(course.find('p'))
+print(course.replace("Python","Java"))
+print(course)
+print(course[6])
+print(course[0:5])
+kg_weight = input("Enter your weight in KGs: ")
+
+g_weght = int(kg_weight)*1000
+print(g_weght)
